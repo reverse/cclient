71c59b6590773ab9e09977930e04f83cc4600849
mckinnonn <mckinnonr03@gmail.com>
2020-10-13 23:38:41 -0400

    add cookieJar support to tls client

--- client.go
+++ client.go
@@ -1,13 +1,30 @@
 package cclient
 
 import (
-	"golang.org/x/net/proxy"
 	"net/http"
 
+	cookiejar "net/http/cookiejar"
+
 	utls "github.com/refraction-networking/utls"
+	"golang.org/x/net/proxy"
 )
 
-func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
+func NewClient(cookieJar *cookiejar.Jar, clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
+	if cookieJar != nil {
+		if len(proxyUrl) > 0 {
+			dialer, err := newConnectDialer(proxyUrl[0])
+			if err != nil {
+				return http.Client{}, err
+			}
+			return http.Client{
+				Transport: newRoundTripper(clientHello, dialer),
+			}, nil
+		}
+		return http.Client{
+			Transport: newRoundTripper(clientHello, proxy.Direct),
+		}, nil
+	}
+
 	if len(proxyUrl) > 0 {
 		dialer, err := newConnectDialer(proxyUrl[0])
 		if err != nil {
@@ -15,10 +32,12 @@ func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client,
 		}
 		return http.Client{
 			Transport: newRoundTripper(clientHello, dialer),
-		}, nil
-	} else {
-		return http.Client{
-			Transport: newRoundTripper(clientHello, proxy.Direct),
+			Jar:       cookieJar,
 		}, nil
 	}
+	return http.Client{
+		Transport: newRoundTripper(clientHello, proxy.Direct),
+		Jar:       cookieJar,
+	}, nil
+
 }
